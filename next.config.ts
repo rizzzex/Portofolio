47baca32922bcc71d92fa57158bf2ea2478033e5
Rizkulloh <rizkullahalfriansyah92@gmail.com>
2026-06-30 21:05:46 +0700

    add

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.37.1', 'localhost'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  output: "standalone",
+  experimental: {
+    cpus: 1,
+  },
 };
 
 export default nextConfig;
